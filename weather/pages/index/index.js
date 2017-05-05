f1f9412471822b70a08dfd9dbc37c792293cb181
wb-sai.hes <wb-sai.hes@alibaba-inc.com>
2017-05-05 14:26:10 +0800

    微信小程序
    微信小程序初次体验

---
+++ weather/pages/index/index.js
@@ -0,0 +1,22 @@
+//index.js
+//获取应用实例
+var util=require('../../utils/util.js');
+Page({
+  data: {
+    weather: {}
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    util.loadWeatherData(function(data){
+      //更新数据
+      that.setData({
+        weather:data
+      });
+      wx.setNavigationBarTitle({
+        title: data.city + ' 天气预报',
+        
+      })
+    })
+  }
+})
