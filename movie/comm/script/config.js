90dd362694869141c4bf5d5fa4202ddbdfaad16e
shuang <shuang_113@qq.com>
2017-07-15 20:38:36 +0800

    modify

--- movie/comm/script/config.js
+++ movie/comm/script/config.js
@@ -36,26 +36,5 @@ module.exports = {
     { type: 'film', id: '26630781', imgUrl: url + '/images/banner_3.jpg' },
     { type: 'film', id: '26415200', imgUrl: url + '/images/banner_4.jpg' },
     { type: 'film', id: '3025375', imgUrl: url + '/images/banner_5.jpg' }
-  ],
-  skinList: [
-    { title: '公路', imgUrl: url + '/images/user_bg_1.jpg' },
-    { title: '黑夜森林', imgUrl: url + '/images/user_bg_2.jpg' },
-    { title: '鱼与水', imgUrl: url + '/images/user_bg_3.jpg' },
-    { title: '山之剪影', imgUrl: url + '/images/user_bg_4.jpg' },
-    { title: '火山', imgUrl: url + '/images/user_bg_5.jpg' },
-    { title: '科技', imgUrl: url + '/images/user_bg_6.jpg' },
-    { title: '沙漠', imgUrl: url + '/images/user_bg_7.jpg' },
-    { title: '叶子', imgUrl: url + '/images/user_bg_8.jpg' },
-    { title: '早餐', imgUrl: url + '/images/user_bg_9.jpg' },
-    { title: '英伦骑车', imgUrl: url + '/images/user_bg_10.jpg' },
-    { title: '草原', imgUrl: url + '/images/user_bg_11.jpg' },
-    { title: '城市', imgUrl: url + '/images/user_bg_12.jpg' }
-  ],
-  shakeSound: {
-    startUrl: url + '/sound/shake.mp3',
-    start: '',
-    completeUrl: url + '/sound/shakeComplete.wav',
-    complete: ''
-  },
-  shakeWelcomeImg: url + '/images/shake_welcome.png'
+  ]
 }
